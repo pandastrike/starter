6ecc627e41886dce59405a96020d52af3da9a018
Matthew King <automatthew@gmail.com>
2012-09-14 12:08:41 -0500

    reorg file for clarity

--- lib/starter/tasks/github.rb
+++ lib/starter/tasks/github.rb
@@ -1,59 +1,5 @@
 require "starter/tasks/starter"
 
-task "github_password" => "github_settings" do
-  require "starter/password"
-  if $STARTER[:settings][:github][:password] == nil
-    password = Starter::Password.request("GitHub")
-    $STARTER[:settings][:github][:password] = password
-  end
-end
-
-task "read_settings" do
-  require "yaml"
-  begin
-    $STARTER[:settings] = YAML.load_file("settings.yml")
-  rescue Errno::ENOENT
-    $stderr.puts "You do not appear to have a settings.yml file."
-    if Starter::Prompt.confirm("Create a stubbed settings file?")
-      File.open("settings.yml", "w") do |f|
-        settings = {
-          :github => {
-            :user => "YOURUSERNAME", :repo => {:owner => "OWNERNAME", :name => $STARTER[:directory]}
-          }
-        }
-        YAML.dump(settings, f)
-        puts "Created settings.yml. Now go edit it and add it to .gitignore."
-      end
-    end
-    exit
-  end
-end
-
-
-task "github_settings" => "read_settings" do
-  if $STARTER[:settings][:github] == nil
-    $stderr.puts "Looks like your settings.yml file isn't set up with a github stanza."
-    exit
-  end
-end
-
-task "github_repo" => %w[ github_settings github_password ] do
-  require 'ghee'
-
-  settings = $STARTER[:settings][:github]
-  user, password, repo = settings.values_at(:user, :password, :repo)
-  ghee = Ghee.basic_auth(user,password)
-
-  repo = ghee.repos(repo[:owner], repo[:name])
-  if repo["message"]
-    puts repo["message"]
-    exit
-  else
-    $STARTER[:github_repo] = repo
-  end
-
-end
-
 desc "Create an issue on GitHub"
 task "github:issue" => "github_repo" do
 
@@ -83,6 +29,64 @@ task "github:issue" => "github_repo" do
     else
       $stdout.puts "Issue ##{result['number']} created."
     end
+  end
+end
+
+
+task "github_repo" => %w[ github_settings github_password ] do
+  require 'ghee'
+
+  settings = $STARTER[:settings][:github]
+  user, password, repo = settings.values_at(:user, :password, :repo)
+  ghee = Ghee.basic_auth(user,password)
+
+  repo = ghee.repos(repo[:owner], repo[:name])
+  if repo["message"]
+    puts repo["message"]
+    exit
+  else
+    $STARTER[:github_repo] = repo
+  end
+
+end
+
+
+task "github_password" => "github_settings" do
+  require "starter/password"
+  if $STARTER[:settings][:github][:password] == nil
+    password = Starter::Password.request("GitHub")
+    $STARTER[:settings][:github][:password] = password
+  end
+end
+
+
+task "read_settings" do
+  require "yaml"
+  begin
+    $STARTER[:settings] = YAML.load_file("settings.yml")
+  rescue Errno::ENOENT
+    $stderr.puts "You do not appear to have a settings.yml file."
+    if Starter::Prompt.confirm("Create a stubbed settings file?")
+      File.open("settings.yml", "w") do |f|
+        settings = {
+          :github => {
+            :user => "YOURUSERNAME",
+            :repo => {:owner => "OWNERNAME", :name => $STARTER[:directory]}
+          }
+        }
+        YAML.dump(settings, f)
+        puts "Created settings.yml. Now go edit it and add it to .gitignore."
+      end
+    end
+    exit
+  end
+end
+
+
+task "github_settings" => "read_settings" do
+  if $STARTER[:settings][:github] == nil
+    $stderr.puts "Looks like your settings.yml file isn't set up with a github stanza."
+    exit
   end
 end
 
